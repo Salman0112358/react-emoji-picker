2ee1488e2b28a65624cc86d57de5f8d3c023dc05
Salman H <Salman0112358@outlook.com>
2022-07-21 13:06:44 +0100

    passing prettier format check

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,16 @@ import EmojiButton from "./components/EmojiButton";
 import PageFooter from "./components/PageFooter";
 import { useState } from "react";
 
-
 function App(): JSX.Element {
   return (
     <>
-    <PageFooter/>
-    <main>
-      <section>
-        <EmojiButton/>
-      </section>
-    </main>
+      <PageFooter />
+      <main>
+        <section>
+          <EmojiButton />
+        </section>
+      </main>
     </>
-
-   
   );
 }
 
