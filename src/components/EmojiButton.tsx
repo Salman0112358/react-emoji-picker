07685528adb38195ead7566ebf9dd1e73dd21d16
Salman H <Salman0112358@outlook.com>
2022-07-21 13:16:31 +0100

    applying prettier again

--- src/components/EmojiButton.tsx
+++ src/components/EmojiButton.tsx
@@ -1,63 +1,73 @@
-import {useState} from 'react'
-const emojiArray : string[] = []
+import { useState } from "react";
+const emojiArray: string[] = [];
 
-export default function EmojiButton () : JSX.Element {
+export default function EmojiButton(): JSX.Element {
+  const [currentEmoji, nextEmoji] = useState("No Emoji has been chosen so far");
+  const [PreviousEmoji, nextPreviousEmoji] = useState(
+    "No Emojis have been chosen so far"
+  );
 
-    const [currentEmoji, nextEmoji] = useState("No Emoji has been chosen so far")
-    const [PreviousEmoji, nextPreviousEmoji] = useState("No Emojis have been chosen so far")
+  const [currentArray, nextArray] = useState<string[]>(emojiArray);
 
+  const whaleEmoji = () => {
+    nextPreviousEmoji(currentEmoji);
+    nextEmoji("🐋");
+    emojiArray.push("🐋");
+    nextArray(emojiArray);
+  };
+  const coffeeEmoji = () => {
+    nextPreviousEmoji(currentEmoji);
+    nextEmoji("☕");
+    emojiArray.push("☕");
+    nextArray(emojiArray);
+  };
+  const dogEmoji = () => {
+    nextPreviousEmoji(currentEmoji);
+    nextEmoji("🐶");
+    emojiArray.push("🐶");
+    nextArray(emojiArray);
+  };
+  const unicornEmoji = () => {
+    nextPreviousEmoji(currentEmoji);
+    nextEmoji("🦄");
+    emojiArray.push("🦄");
+    nextArray(emojiArray);
+  };
+  const goatEmoji = () => {
+    nextPreviousEmoji(currentEmoji);
+    nextEmoji("🐐");
+    emojiArray.push("🐐");
+    nextArray(emojiArray);
+  };
 
-    const [currentArray, nextArray] = useState<string[]>(emojiArray)
+  console.log(emojiArray);
+  console.log(`${currentArray} this is the previous array `);
 
-    const whaleEmoji = () => {
-        nextPreviousEmoji(currentEmoji)
-        nextEmoji("🐋")
-        emojiArray.push("🐋")
-        nextArray(emojiArray)
-        
-    }
-    const coffeeEmoji = () => {
-        nextPreviousEmoji(currentEmoji)
-        nextEmoji("☕")
-        emojiArray.push("☕")
-        nextArray(emojiArray)
-    }
-    const dogEmoji = () => {
-        nextPreviousEmoji(currentEmoji)
-        nextEmoji("🐶")
-        emojiArray.push("🐶")
-        nextArray(emojiArray)
-    }
-    const unicornEmoji = () => {
-        nextPreviousEmoji(currentEmoji)
-        nextEmoji("🦄")
-        emojiArray.push("🦄")
-        nextArray(emojiArray)
-    }
-    const goatEmoji = () => {
-        nextPreviousEmoji(currentEmoji)
-        nextEmoji("🐐")
-        emojiArray.push("🐐")
-        nextArray(emojiArray)
-    }
-
-    console.log(emojiArray)
-    console.log(`${currentArray} this is the previous array `)
-    
-    return (
-        <>
-        <button onClick={whaleEmoji} className="emoji-btn">{"🐋"}</button>
-        <button onClick={coffeeEmoji} className="emoji-btn">{"☕"}</button>
-        <button onClick={dogEmoji} className="emoji-btn">{"🐶"}</button>
-        <button onClick={unicornEmoji} className="emoji-btn">{"🦄"}</button>
-        <button onClick={goatEmoji} className="emoji-btn">{"🐐"}</button>
-        <section>
+  return (
+    <>
+      <button onClick={whaleEmoji} className="emoji-btn">
+        {"🐋"}
+      </button>
+      <button onClick={coffeeEmoji} className="emoji-btn">
+        {"☕"}
+      </button>
+      <button onClick={dogEmoji} className="emoji-btn">
+        {"🐶"}
+      </button>
+      <button onClick={unicornEmoji} className="emoji-btn">
+        {"🦄"}
+      </button>
+      <button onClick={goatEmoji} className="emoji-btn">
+        {"🐐"}
+      </button>
+      <section>
         <h2>Current Emoji: {currentEmoji}</h2>
         <h2>Previous Emoji: {PreviousEmoji} </h2>
-        <h2>Previous 2 Emojis: {PreviousEmoji}, {currentEmoji}  </h2>
-        <h2>Previous Emojis: {currentArray}  </h2>
-        </section>
-        </>
-
-    )
+        <h2>
+          Previous 2 Emojis: {PreviousEmoji}, {currentEmoji}{" "}
+        </h2>
+        <h2>Previous Emojis: {currentArray} </h2>
+      </section>
+    </>
+  );
 }
